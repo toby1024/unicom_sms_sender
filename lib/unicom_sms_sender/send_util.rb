4ebd4f204e44fee4fc405900d6709bee767cc836
jason <zhangbin@skio.cn>
2016-09-24 23:13:34 +0800

    send sms

---
+++ lib/unicom_sms_sender/send_util.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+module UnicomSmsSender
+  class SendUtil
+    attr_reader :app_name, :message_type, :phone, :content
+
+    class << self
+      def [](app_name, message_type, phone, content)
+        SendUtil.new(app_name, message_type, phone, content)
+      end
+    end
+
+    def initialize(app_name, message_type, phone, content)
+      @app_name = app_name
+      @message_type = message_type
+      @phone = phone
+      @content = content
+    end
+
+    def send
+      send_sms(path, content)
+    end
+
+    private
+    def configuration
+      UnicomSmsSender.configuration
+    end
+
+    def send_sms(phone, message)
+      require "base64"
+      require "erb"
+      service_id = configuration.send_service_id
+      send_key = configuration.send_send_key
+      cp_id = configuration.send_cpid
+      url = configuration.send_send_url
+
+      sign = Digest::MD5.hexdigest("#{cp_id}#{send_key}")
+      message = Base64.strict_encode64(message)
+      agent = Mechanize.new
+      page = agent.post(url, {
+          "mobi" => phone,
+          "msg" => message,
+          "sign" => sign,
+          "sid" => service_id,
+      })
+      page.body.to_s
+    end
+  end
+
+end
